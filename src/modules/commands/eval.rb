a86b8a2f1cb197f4281e6ebc001aea4a377b3eab
Faisa <75602020+justfaisa@users.noreply.github.com>
2021-07-23 11:09:46 +0700

    Create eval.rb

---
+++ src/modules/commands/eval.rb
@@ -0,0 +1,13 @@
+module Bot::DiscordCommands
+  module Eval
+    extend Discordrb::Commands::CommandContainer
+    command(:eval, help_available: false) do |event, *code|
+      break unless event.user.id == Bot::CONFIG.owner
+      begin
+        eval code.join(' ')
+      rescue => e # Return error
+        "An error occurred 😞 ```#{e}```"
+      end
+    end
+  end
+end
