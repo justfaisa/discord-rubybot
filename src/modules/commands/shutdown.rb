ee0f8e7acc154edd0e9a9e8bfb486d34cdec40cc
Faisa <faiisan645@gmail.com>
2021-08-14 07:25:59 +0700

    Update shutdown.rb

--- src/modules/commands/shutdown.rb
+++ src/modules/commands/shutdown.rb
@@ -1,7 +1,7 @@
 module Bot::DiscordCommands
   module Shutdown
     extend Discordrb::Commands::CommandContainer
-    command(:exit, help_available: false) do |event|
+    command :exit do |event|
       break unless event.user.id == Bot::CONFIG.owner
       'Bot is shutting down'
     end
